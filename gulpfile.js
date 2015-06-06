136e70166d9a4bae26d38f77519931d03181261a
therealmark <markstevenfranklin@gmail.com>
2015-06-05 19:57:21 -0700

    added minification to gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var uglify = require('gulp-uglify');
 
 gulp.task("default", function () {
   return gulp.src("app/**/*.js")
     .pipe(babel())
+    .pipe(uglify())
     .pipe(gulp.dest("public/javascripts"));
 });
+
