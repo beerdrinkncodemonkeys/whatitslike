dc384505b65415b89327b1e4c31f8c1cfa4036da
therealmark <markstevenfranklin@gmail.com>
2015-06-05 20:02:18 -0700

    deleted all the js in the dis folder as this is effectivly bytecode for the web

--- public/javascripts/main.js
+++
@@ -1,1 +0,0 @@
-"use strict";var _Message=require("./message.html"),_Navbar=require("./navbar.html"),_Pulse=require("./pulse.html"),_Beat=require("./beat.html");$(function(){var e=new _Message.Message,a=new _Beat.Beat,p=new _Navbar.Navbar,r=new _Pulse.Pulse;$("#app").append(p.el),$("#app").append(r.el),$("#app").append(e.el),$("#pulse-1").append(a.el)});
