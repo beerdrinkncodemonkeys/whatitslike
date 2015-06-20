2644b49f1e35add5c2d0072d90f51bbb8d9f151d
therealmark <markstevenfranklin@gmail.com>
2015-06-19 21:21:17 -0700

    removed extra return statement in puls.js

--- app/javascripts/pulse.js
+++ app/javascripts/pulse.js
@@ -6,7 +6,7 @@ export class Pulse extends React.Component {
 
     render() {
 
-        return <div className="pulse text-center"><h1>Crime Pulse</h1><hr></hr></div>;
+        
         return React.createElement("div", {className: "pulse text-center"},
                     React.createElement("h1", null, "Crime Pulse", 
                         React.createElement("hr", null, null)
