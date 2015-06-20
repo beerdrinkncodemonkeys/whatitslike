fe87236f029781bd63e2026e6461a60adad55efd
therealmark <markstevenfranklin@gmail.com>
2015-06-19 21:20:16 -0700

    removed jsx transformer and rolled vanilla js

--- app/javascripts/pulse.js
+++ app/javascripts/pulse.js
@@ -2,13 +2,16 @@
 
 import React from 'react';
 
-import $ from 'jquery';
-
 export class Pulse extends React.Component {
 
     render() {
 
         return <div className="pulse text-center"><h1>Crime Pulse</h1><hr></hr></div>;
+        return React.createElement("div", {className: "pulse text-center"},
+                    React.createElement("h1", null, "Crime Pulse", 
+                        React.createElement("hr", null, null)
+                    )
+            );
     }
 
 }
